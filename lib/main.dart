63b3e01aec9ede60b6ff6a0ff69bdcfadab3cb16
Truong Hoang Phi <phi.th04840@sinhvien.hoasen.edu.vn>
2025-05-11 16:52:48 +0700

    Sets up initial project structure
    Initializes the Flutter project with basic app structure, theming and routing.
    
    - Adds GetX for state management and navigation.
    - Creates initial routes and pages.
    - Implements custom theme with light and dark color schemes.
    - Configures Material 3 theming.

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,34 @@
 import 'package:flutter/material.dart';
-
+import 'package:calliope/share/theme.dart';
 import 'package:get/get.dart';
 
 import 'app/routes/app_pages.dart';
 
 void main() {
   runApp(
-    GetMaterialApp(
-      title: "Application",
+    //Gắn database ở đây
+
+    MyApp(),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  TextTheme textTheme = TextTheme(
+    displayLarge: TextStyle(fontSize: 72.0, fontWeight: FontWeight.bold),
+    displayMedium: TextStyle(fontSize: 36.0, fontStyle: FontStyle.italic),
+    bodyLarge: TextStyle(fontSize: 14.0, fontFamily: 'Lexend'),
+    // Add other text styles as needed
+  );
+  MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Calliope',
+      theme: MaterialTheme(textTheme).light(),
       initialRoute: AppPages.INITIAL,
       getPages: AppPages.routes,
-    ),
-  );
+    );
+  }
 }
