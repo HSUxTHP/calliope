189f6af55ccacb57985370e0a233e98d4881d326
Truong Hoang Phi <phi.th04840@sinhvien.hoasen.edu.vn>
2025-05-25 14:25:30 +0700

    Configures Firebase and Supabase integration
    Sets up Firebase and Supabase for the project. This includes:
    
    - Adding necessary dependencies and configuration files for Firebase integration, enabling features like authentication and data storage.
    - Configuring Supabase to manage the app's backend services.
    - Initializing Firebase and Supabase in the main application file.
    - Adding .env file to gitignore.

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,22 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:calliope/share/theme.dart';
 import 'package:get/get.dart';
-
+import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'app/routes/app_pages.dart';
+import 'firebase_options.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  await dotenv.load(fileName: ".env");
+  await Supabase.initialize(
+    url: dotenv.env['SUPABASE_URL']!,
+    anonKey: dotenv.env['SUPABASE_ANON_KEY']!,
+  );
   runApp(
     //Gắn database ở đây
 
