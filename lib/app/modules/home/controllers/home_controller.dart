ddc99db53f9ef8c4a69aa6547a393725172e6864
Tamek <jameanderson2004@gmail.com>
2025-05-28 08:35:04 +0700

    Feat: Home page UI complete, create project dialog added

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -4,6 +4,11 @@ class HomeController extends GetxController {
   //TODO: Implement HomeController
 
   final count = 0.obs;
+
+  final List<int> fpsOptions = [1, 2, 3, 4, 5];
+  final List<int> onionSkinOptions = [0, 1, 2, 3];
+  final fps = 3.obs;
+  final onionSkin = 1.obs;
   @override
   void onInit() {
     super.onInit();
--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -83,7 +83,89 @@ class HomeView extends GetView<HomeController> {
                           ),
                           backgroundColor: Theme.of(context).colorScheme.primary,
                         ),
-                        onPressed: () {},
+                        onPressed: () {
+                          showDialog(
+                              context: context,
+                              builder: (BuildContext) {
+                                return Dialog(
+                                  backgroundColor: Theme.of(context).colorScheme.surface,
+                                  child: SizedBox(
+                                    width: MediaQuery.sizeOf(context).width * 0.6,
+                                    height: MediaQuery.sizeOf(context).height * 0.6,
+                                    child: Padding(
+                                      padding: const EdgeInsets.all(24.0),
+                                      child: Column(
+                                        crossAxisAlignment: CrossAxisAlignment.start,
+                                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                        children: [
+                                          Text(
+                                              "Create a new project",
+                                              style: TextStyle(
+                                                fontSize: 16,
+                                                color: Theme.of(context).colorScheme.onSurface,
+                                                fontWeight: FontWeight.bold,
+                                              )
+                                          ),
+                                          SizedBox(
+                                            width: double.infinity,
+                                            height: MediaQuery.sizeOf(context).height * 0.4,
+                                            child: SingleChildScrollView(
+                                              child: Column(
+                                                spacing: 16,
+                                                children: [
+                                                  TextField(
+                                                    decoration: InputDecoration(
+                                                      labelText: 'Your project name',
+                                                      border: OutlineInputBorder(),
+                                                    ),
+                                                  ),
+                                                  _buildDropdownTile(
+                                                    context: context,
+                                                    title: "Frames Per Second (FPS)",
+                                                    subtitle: "Customize the number of frames displayed per second to control the animation playback speed.",
+                                                    valueRx: controller.fps,
+                                                    items: controller.fpsOptions,
+                                                  ),
+                                                  _buildDropdownTile(
+                                                    context: context,
+                                                    title: "Onion Skin",
+                                                    subtitle: "Show previous and next frames for smoother drawing between frames",
+                                                    valueRx: controller.onionSkin,
+                                                    items: controller.onionSkinOptions,
+                                                  ),
+
+
+                                                ],
+                                              ),
+                                            ),
+                                          ),
+                                          Row(
+                                            mainAxisAlignment: MainAxisAlignment.end,
+                                            children: [
+                                              TextButton(
+                                                onPressed: () {
+                                                  Navigator.of(context).pop();
+                                                },
+                                                child: const Text("Cancel"),
+                                              ),
+                                              const SizedBox(width: 8),
+                                              FilledButton(
+                                                onPressed: () {
+                                                  // Handle project creation logic here
+                                                  Navigator.of(context).pop();
+                                                },
+                                                child: const Text("Create"),
+                                              ),
+                                            ],
+                                          )
+                                        ],
+                                      ),
+                                    )
+                                  ),
+                                );
+                              }
+                          );
+                        },
                         child: Column(
                           mainAxisAlignment: MainAxisAlignment.center,
                           children: [
@@ -102,7 +184,73 @@ class HomeView extends GetView<HomeController> {
                         ),
                       ),
                     ),
+                    Container(
+                      padding: const EdgeInsets.all(0),
+                      decoration: BoxDecoration(
+                        border: Border(
+                         bottom: BorderSide(
+                          color: Theme.of(context).colorScheme.onSurface,
+                          width: 1.0, // Adjust the width as needed
+                         )
+                        )
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
 
+                          Padding(
+                            padding: const EdgeInsets.symmetric(vertical: 8),
+                            child: Row(
+                              spacing: 16,
+                              children: [
+                                Icon(
+                                  Icons.brush,
+                                  size: 44,
+                                  color: Theme.of(context).colorScheme.onSurface,
+                                ),
+                                Text(
+                                    "Your Project",
+                                    style: TextStyle(
+                                      fontSize: 36,
+                                      color: Theme.of(context).colorScheme.onSurface,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                )
+                              ],
+                            ),
+                          ),
+                          // Search Bar
+                          Padding(
+                            padding: const EdgeInsets.symmetric(vertical: 8),
+                            child: SizedBox(
+                              width: MediaQuery.sizeOf(context).width * 0.3,
+                              child: TextField(
+                                decoration: InputDecoration(
+                                  hintStyle: TextStyle(
+                                    fontSize: 16
+                                  ),
+                                  hintText: 'Search your project here',
+                                  prefixIcon: const Icon(Icons.search),
+                                  contentPadding: const EdgeInsets.symmetric(vertical: 0, horizontal: 16),
+                                  filled: true,
+                                  fillColor: Colors.white,
+                                  border: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(8),
+                                    borderSide: const BorderSide(
+                                      color: Colors.black, // Border color
+                                      width: 1.0,         // Border width
+                                    ),
+                                  ),
+                                ),
+                                onSubmitted: (value) {
+                                  print('Search submitted: $value');
+                                },
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
                     GridView.builder(
                       shrinkWrap: true,
                       physics: const NeverScrollableScrollPhysics(),
@@ -112,7 +260,7 @@ class HomeView extends GetView<HomeController> {
                         mainAxisSpacing: 12,
                         childAspectRatio: 1,
                       ),
-                      itemCount: 10, // Add itemCount
+                      itemCount: 20, // Add itemCount
                       itemBuilder: (context, index) {
                         return ProjectCard(
                           imageUrl: "https://miro.medium.com/v2/resize:fit:1200/1*uNCVd_VqFOcdxhsL71cT5Q.jpeg",
@@ -131,5 +279,58 @@ class HomeView extends GetView<HomeController> {
     );
   }
 
+  Widget _buildDropdownTile({
+    required BuildContext context,
+    required String title,
+    required String subtitle,
+    required RxInt valueRx,
+    required List<int> items,
+  }) {
+    return Obx(() => Container(
+      padding: EdgeInsets.all(12),
+      decoration: BoxDecoration(
+        color: Theme.of(context).colorScheme.surfaceContainer,
+        borderRadius: BorderRadius.circular(8),
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        spacing: 8,
+        children: [
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(title, style: TextStyle(fontWeight: FontWeight.bold)),
+                const SizedBox(height: 4),
+                Text(subtitle, style: TextStyle(fontSize: 12)),
+                const SizedBox(height: 8),
+              ],
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 8),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(8),
+              border: Border.all(
+                color: Colors.black, // Border color
+                width: 3.0,          // Border width
+              ),
+            ),
+            child: DropdownButton<int>(
+              value: valueRx.value,
+              isExpanded: false,
+              onChanged: (value) => valueRx.value = value!,
+              items: items.map((val) {
+                return DropdownMenuItem<int>(
+                  value: val,
+                  child: Text(val.toString()),
+                );
+              }).toList(),
+            ),
+          ),
+        ],
+      ),
+    ));
+  }
 
 }
