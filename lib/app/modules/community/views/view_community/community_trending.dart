631cb8a87f09ef406448049b4587794beeae2c37
Tamek <jameanderson2004@gmail.com>
2025-06-01 14:18:58 +0700

    Feat: Seperate Create Dialog, Add popupmenu

--- lib/app/modules/community/views/view_community/community_trending.dart
+++ lib/app/modules/community/views/view_community/community_trending.dart
@@ -1,5 +1,6 @@
 import 'package:calliope/app/modules/community/controllers/community_controller.dart';
 import 'package:calliope/app/widget_share/post_widget.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:get/get.dart';
 
@@ -7,24 +8,29 @@ class TrendingCommunity extends GetView<CommunityController> {
   const TrendingCommunity({super.key});
   @override
   Widget build(BuildContext context) {
-    return GridView.builder(
-      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 3,
-        childAspectRatio: 1,
-        crossAxisSpacing: 10,
-        mainAxisSpacing: 10,
-      ),
-      itemCount: 20,
-      itemBuilder: (context, index) {
-        return PostCard(
-            imageUrl: "assets/video_cover_example.png",
-            title: "Project that i made by myself absolutely",
-            avatarUrl: "assets/avatar.png",
-            userName: "Username1",
-            createdAt: "2023-10-01",
-            views: "0"
-        );
+    return RefreshIndicator(
+      onRefresh: () async {
+        await Future.delayed(const Duration(seconds: 1));
       },
+      child: GridView.builder(
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 3,
+          childAspectRatio: 1,
+          crossAxisSpacing: 10,
+          mainAxisSpacing: 10,
+        ),
+        itemCount: 20,
+        itemBuilder: (context, index) {
+          return PostCard(
+              imageUrl: "assets/video_cover_example.png",
+              title: "Project that i made by myself absolutely",
+              avatarUrl: "assets/avatar.png",
+              userName: "Username1",
+              createdAt: "2023-10-01",
+              views: "0"
+          );
+        },
+      ),
     );
   }
 }
--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,4 +1,5 @@
 import 'package:calliope/app/modules/draw/views/draw_view.dart';
+import 'package:calliope/app/modules/home/views/create_project_dialog.dart';
 import 'package:calliope/app/modules/layout/controllers/layout_controller.dart';
 import 'package:calliope/app/modules/layout/views/ProjectCard.dart';
 import 'package:flutter/material.dart';
@@ -93,84 +94,7 @@ class HomeView extends GetView<HomeController> {
                         onPressed: () {
                           showDialog(
                               context: context,
-                              builder: (BuildContext) {
-                                return Dialog(
-                                  backgroundColor: Theme.of(context).colorScheme.surface,
-                                  child: SizedBox(
-                                    width: MediaQuery.sizeOf(context).width * 0.6,
-                                    height: MediaQuery.sizeOf(context).height * 0.6,
-                                    child: Padding(
-                                      padding: const EdgeInsets.all(24.0),
-                                      child: Column(
-                                        crossAxisAlignment: CrossAxisAlignment.start,
-                                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                        children: [
-                                          Text(
-                                              "Create a new project",
-                                              style: TextStyle(
-                                                fontSize: 16,
-                                                color: Theme.of(context).colorScheme.onSurface,
-                                                fontWeight: FontWeight.bold,
-                                              )
-                                          ),
-                                          SizedBox(
-                                            width: double.infinity,
-                                            height: MediaQuery.sizeOf(context).height * 0.4,
-                                            child: SingleChildScrollView(
-                                              child: Column(
-                                                spacing: 16,
-                                                children: [
-                                                  TextField(
-                                                    decoration: InputDecoration(
-                                                      labelText: 'Your project name',
-                                                      border: OutlineInputBorder(),
-                                                    ),
-                                                  ),
-                                                  _buildDropdownTile(
-                                                    context: context,
-                                                    title: "Frames Per Second (FPS)",
-                                                    subtitle: "Customize the number of frames displayed per second to control the animation playback speed.",
-                                                    valueRx: controller.fps,
-                                                    items: controller.fpsOptions,
-                                                  ),
-                                                  _buildDropdownTile(
-                                                    context: context,
-                                                    title: "Onion Skin",
-                                                    subtitle: "Show previous and next frames for smoother drawing between frames",
-                                                    valueRx: controller.onionSkin,
-                                                    items: controller.onionSkinOptions,
-                                                  ),
-
-
-                                                ],
-                                              ),
-                                            ),
-                                          ),
-                                          Row(
-                                            mainAxisAlignment: MainAxisAlignment.end,
-                                            children: [
-                                              TextButton(
-                                                onPressed: () {
-                                                  Navigator.of(context).pop();
-                                                },
-                                                child: const Text("Cancel"),
-                                              ),
-                                              const SizedBox(width: 8),
-                                              FilledButton(
-                                                onPressed: () {
-                                                  // Handle project creation logic here
-                                                  Navigator.of(context).pop();
-                                                },
-                                                child: const Text("Create"),
-                                              ),
-                                            ],
-                                          )
-                                        ],
-                                      ),
-                                    )
-                                  ),
-                                );
-                              }
+                              builder: (context) => CreateProjectDialog(controller: controller),
                           );
                         },
                         child: Column(
@@ -286,61 +210,6 @@ class HomeView extends GetView<HomeController> {
         ],
       ),
     );
-  }
-
-  Widget _buildDropdownTile({
-    required BuildContext context,
-    required String title,
-    required String subtitle,
-    required RxInt valueRx,
-    required List<int> items,
-  }) {
-    return Obx(() => Container(
-      padding: EdgeInsets.all(12),
-      decoration: BoxDecoration(
-        color: Theme.of(context).colorScheme.surfaceContainer,
-        borderRadius: BorderRadius.circular(8),
-      ),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        spacing: 8,
-        children: [
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(title, style: TextStyle(fontWeight: FontWeight.bold)),
-                const SizedBox(height: 4),
-                Text(subtitle, style: TextStyle(fontSize: 12)),
-                const SizedBox(height: 8),
-              ],
-            ),
-          ),
-          Container(
-            padding: EdgeInsets.symmetric(horizontal: 8),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(8),
-              border: Border.all(
-                color: Theme.of(context).colorScheme.onSurface, // Border color
-                width: 2.0,          // Border width
-              ),
-            ),
-            child: DropdownButton<int>(
-
-              value: valueRx.value,
-              isExpanded: false,
-              onChanged: (value) => valueRx.value = value!,
-              items: items.map((val) {
-                return DropdownMenuItem<int>(
-                  value: val,
-                  child: Text(val.toString()),
-                );
-              }).toList(),
-            ),
-          ),
-        ],
-      ),
-    ));
   }
 
 }
